85720db7f547ac1e81ee40fc1a82236adc4b5859
bartwitting <bartwitting@icloud.com>
2020-02-21 12:36:10 +0100

    Laatste aanpassing voting

--- Voting/voting.js
+++ Voting/voting.js
@@ -3,22 +3,20 @@ var votingBib = {};
 var winnerVoting;
 
 function vote(id){
-    
+    console.log(votingBib);
+    if (id in Object.keys(votingBib)) {
+        votingBib[id] +=1
+    }
+    else {
+        votingBib[id] = 0
+    };
     totalVotes += 1;
     updatePoints();
 }
 
 function updatePoints(){
-    // var percentA = (pointA / totalVotes) * 100;
-    // var percentB = (pointB / totalVotes) * 100;
-    // var size = percentA + "% " + percentB + "%";
-
-    // document.getElementById("size-one").innerHTML = Math.round(percentA) + '%';
-    // document.getElementById("size-two").innerHTML = Math.round(percentB) + '%';
-    // document.getElementById("voting-box").style.gridTemplateColumns=  percentA + "% " + percentB + "%";
-    // document.getElementById("total-votes").innerHTML = "Total Votes Casted: " + totalVotes;
-    // document.getElementById("total-left").innerHTML = "Option A: " + pointA;
+    winnerVoting = Object.keys(votingBib).reduce(function(a, b){ return votingBib[a] > votingBib[b] ? a : b });
     document.getElementById("currentVotes").innerHTML = totalVotes;
     document.getElementById("Winner").innerHTML = winnerVoting;
+    //console.log(votingBib);
 }
-
