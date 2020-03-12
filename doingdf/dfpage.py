6fd3d8f9caf83852f722a846f088f1e675a7e485
hiddedehaan <hiddedehaan@hotmail.com>
2020-03-12 14:46:39 +0100

    nieuwe versie website en code

--- doingdf/dfpage.py
+++ doingdf/dfpage.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from flask import Flask, request, render_template, session, redirect
+from flask import Flask
+from OpenSSL import SSL
+import datetime
+from AllCode import *
+now = datetime.datetime.now()
 
 df = pd.read_csv('https://raw.githubusercontent.com/bartwitting/DSCS/master/afspeellijsten/Road.csv')
 df = df[['title','artist']]
 df.set_index(['title'], inplace=True)
-
-from flask import Flask
-from OpenSSL import SSL
-
-
 
 app = Flask(__name__)
 
@@ -18,11 +18,11 @@ def dfpage():
 
     titles = ['na', 'Current playlist'])
 
-@app.route('/SomeFunction')
-def SomeFunction():
-    print('In SomeFunction')
-    return "Nothing"
-
+@app.route("/forward/", methods=['POST'])
+def move_forward():
+    #Moving forward code
+    forward_message = Parameter_ranges(50, 10, 20, 100, 100, 15)
+    return render_template('home.html', forward_message=forward_message);
 
 
 
