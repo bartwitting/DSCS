8b9e8f8a130a4dade07a2ba591af18b486ebdf2b
hiddedehaan <hiddedehaan@hotmail.com>
2020-03-17 17:27:07 +0100

    website update

--- doingdf/dfpage.py
+++ doingdf/dfpage.py
@@ -2,10 +2,10 @@ import pandas as pd
 from flask import Flask, request, render_template, session, redirect
 from flask import Flask
 from OpenSSL import SSL
-from datetime import datetime
+from datetime import datetime as time_
 from datetime import date
 import time
-#from test import *
+from AllCode import *
 #now = datetime.datetime.now()
 
 #df = pd.read_csv('https://raw.githubusercontent.com/bartwitting/DSCS/master/afspeellijsten/Road.csv')
@@ -15,22 +15,27 @@ import time
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def dfpage():
-    return render_template('index.html')
+  if request.method == 'POST':
+
+      Username = request.form['username']
+      Spotifylink = request.form['Spotifylink']
+
+      return redirect(Username.url)
+      #return render_template('index.html',haha1=Username,haha2=Spotifylink)
+  return render_template('index.html')
+
 
 @app.route("/forward/", methods=['POST'])
 def move_forward():
     time.sleep(10)
-    now = datetime.now()
+    now = time_.now()
     forward_message = now
     df = pd.read_csv('https://raw.githubusercontent.com/bartwitting/DSCS/master/afspeellijsten/Road.csv')
     df = df[['title','artist']]
     #df.set_index(['title'], inplace=True)
-    return render_template('home.html', forward_message=forward_message ,tables =[df.to_html(classes='playlist')],titles = ['na', 'Current playlist'])
-
-
-
+    return render_template('home.html', forward_message=forward_message,tables =[df.to_html(classes='playlist')],titles = ['na', 'Current playlist'])
 
 
 if __name__ == '__main__':
