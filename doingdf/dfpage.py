d02b573411a6f2f366f126b580b275b90b28b6f9
hiddedehaan <hiddedehaan@hotmail.com>
2020-03-19 15:16:14 +0100

    website updates iets

--- doingdf/dfpage.py
+++ doingdf/dfpage.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from flask import Flask, request, render_template, session, redirect
-from flask import Flask
+from flask import Flask, request, render_template, session, redirect, url_for
+
 from OpenSSL import SSL
 from datetime import datetime as time_
 from datetime import date
@@ -14,45 +14,41 @@ from AllCode import *
 #text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
-@app.route('/', methods=['POST','GET'])
+
+@app.route('/', methods = ['GET','POST'])
 def dfpage():
-    if request.method == 'POST':
-        print ('hallo!!!')
-        item = request.form['Username']
-        print (item)
-        session["Username"]=item
-        session['Spotifylink']= request.form.get("Spotifylink", 'sukkel')
-        return render_template('index.html')
-    else:
-        print('wtf is dit')
-        return render_template('index.html')
-
-
-      #return render_template('index.html')
-      #return render_template('index.html',haha1=Username,haha2=Spotifylink)
+    return render_template('index.html')
 
 
 
-@app.route("/forward/", methods=['POST','GET'])
+
+@app.route("/forward/", methods=['GET','POST'])
 def move_forward():
     #Username = session['Username']
     #Spotifylink = session['Spotifylink']
     #iliketrains = request.form['iliketrains']
-    Username = session.get('Username')
-    print (Username)
-    Spotifylink=session.get('Spotifylink')
-    now = time_.now()
-    forward_message = now
-    df = pd.read_csv('https://raw.githubusercontent.com/bartwitting/DSCS/master/afspeellijsten/Road.csv')
-    df = df[['title','artist']]
+    if request.method == 'POST':
+        print ('hallo!!!')
+        Spotusername = request.form['Username']
+        Playlink = request.form['Spotifylink']
+        return render_template('playlists.html', forward_message=forward_message,tables =[df.to_html(classes='playlist')],titles = ['na', 'Current playlist'], Username=Spotusername, Spotifylink=Playlink)
 
-    time.sleep(10)
+
+    else:
+
+        now = time_.now()
+        forward_message = now
+        df = pd.read_csv('https://raw.githubusercontent.com/bartwitting/DSCS/master/afspeellijsten/Road.csv')
+        df = df[['title','artist']]
+
+        time.sleep(10)
         #print(brew_type)
-    return render_template('playlists.html', forward_message=forward_message,tables =[df.to_html(classes='playlist')],titles = ['na', 'Current playlist'], Username=Username, Spotifylink=Spotifylink)
+        return render_template('playlists.html', forward_message=forward_message,tables =[df.to_html(classes='playlist')],titles = ['na', 'Current playlist'], Username=Spotusername, Spotifylink=Playlink)
 
-    #df.set_index(['title'], inplace=True)
-    #, Spotifylink=session['Spotifylink'])
+
 
 
 if __name__ == '__main__':
